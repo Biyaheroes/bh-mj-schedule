496eb77377aeb2ce579597cb33c113c0eb60a38a
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-03 17:26:14 +0800

    Update bh-mj-schedule component.

---
+++ component.jsx
@@ -0,0 +1,14 @@
+
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Schedule from "./schedule.js";
+
+ReactDOM.render( 
+	<Schedule
+		label="This is a schedule">
+	</Schedule>,
+	document.getElementById( "root" ) 
+);
--- test.jsx
+++
@@ -1,9 +0,0 @@
-
-"use strict";
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Schedule from "./schedule.js";
-
-ReactDOM.render( <Schedule></Schedule> );
